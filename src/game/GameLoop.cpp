cd543973169cb98bb43cf3a77ed65ef2c3f85fb8
czarny247 <mateusz.hercun@gmail.com>
2020-04-02 17:06:48 +0200

    GameLoop changes - TextureManager added, update and render implemented

--- src/game/GameLoop.cpp
+++ src/game/GameLoop.cpp
@@ -8,7 +8,20 @@ namespace
 namespace game
 {
 
-void GameLoop::run(SDL_Window* window, SDL_Renderer* renderer)
+GameLoop::GameLoop(SDL_Window* window, SDL_Renderer* renderer)
+: window_(window)
+, renderer_(renderer)
+{
+	textureManager_.load("../res/rogue.png", "rogue", renderer);
+}
+
+GameLoop::~GameLoop()
+{
+	window_ = nullptr;
+	renderer_ = nullptr;
+}
+
+void GameLoop::run()
 {
 	auto previousTime = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> lag {0.0};
@@ -23,11 +36,12 @@ void GameLoop::run(SDL_Window* window, SDL_Renderer* renderer)
 
 		while(lag >= MS_PER_UPDATE)
 		{
-			//update
+			//update();
 			lag -= MS_PER_UPDATE;
 		}
 
 		//render(lag / MS_PER_UPDATE)
+		//render(lag.count() / MS_PER_UPDATE.count());
 	}
 }
 
@@ -40,6 +54,20 @@ void GameLoop::processInput(bool& isQuitEvent)
 			isQuitEvent = true;
 		}
 	}
+}
+
+void GameLoop::render(double movementSpeedFactor)
+{
+	SDL_RenderClear(renderer_);
+	textureManager_.draw("rogue", 0,0, 128, 82, renderer_);
+	textureManager_.drawFrame("rogue", 
+		100*movementSpeedFactor, 100*movementSpeedFactor, 128, 82, 1, currentFrame_, renderer_);
+    SDL_RenderPresent(renderer_);
+}
+
+void GameLoop::update()
+{
+	currentFrame_ = int(((SDL_GetTicks() / 100) % 6));
 }
 
 }
